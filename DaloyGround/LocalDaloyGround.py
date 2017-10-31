44112b7e90ebac062b36579c4888c25b044eaacc
BuildTools <unconfigured@null.spigotmc.org>
2017-10-31 14:58:27 +0800

    Tidied up code in LocalDaloyGround

--- DaloyGround/LocalDaloyGround.py
+++ DaloyGround/LocalDaloyGround.py
@@ -3,73 +3,67 @@ import time
 import commands
 
 leds = [11, 13, 19, 21, 23]
-duration = 1
 # red, green, rgb_red, rgb_green, rgb_blue
+duration = 0.5
 
-def led(pin, on=True):
-    GPIO.output(pin, on)
+def delay(n):
+	time.sleep(n)
 
-def blink(pin, duration):
-    led(pin)
-    time.sleep(duration)
-    led(pin, False)
-    time.sleep(duration)
-    
-def blinkMulti(pins, duration):
-    for i in pins:
-        led(i)
-    time.sleep(duration)
-    for i in pins:
-        led(i, False)
-    time.sleep(duration)
-    
-def blinkNum(pow10, val):
-    global duration
-    if pow10 == 0:
-        # red
-        for i in range(val):
-            blink(leds[2], duration)
-    elif pow10 == 1:
-        # green
-        for i in range(val):
-            blink(leds[3], duration)
-    elif pow10 == 2:
-        # blue
-        for i in range(val):
-            blink(leds[4], duration)
-    else:
-        # anuna
-        blink(leds[0], duration)
-    # white
-    blinkMulti(leds[2:5], duration)
+def led(pins, on = True):
+	if pins is not list:
+		pins = [pins]
+	for pin in pins:
+		GPIO.output(pin, on)
 
-# startup
-GPIO.setmode(GPIO.BOARD)
-for l in leds:
-    GPIO.setup(l, GPIO.OUT)
-led(leds[0])
-time.sleep(1)
+def blink(pins, count = 0):
+	global duration
+	if pins is not list:
+		pins = [pins]
+	for i in range(count):
+		for pin in pins:
+			GPIO.output(pin, True)
+		delay(duration)
+		for pin in pins:
+			GPIO.output(pin, False)
+		delay(duration)
 
-f = open("test.txt", "w")
-f.write("sample words\n")
-f.write(str(time.time()))
-f.close()
-ipstr = commands.getoutput("hostname -I")
-octets = [int(i) for i in ipstr.split(".")]
-for octet in octets:
-    blinkMulti(leds, duration)
-    for i in range(octet):
-        hundreds = i / 100
-        tens = (i % 100) / 10
-        ones = (i % 10)
-        blinkNum(2, hundreds)
-        blinkNum(1, tens)
-        blinkNum(0, ones)
-        
-# blink for ip address
+def startup():
+	global duration
+	GPIO.setmode(GPIO.BOARD)
+	for l in leds:
+		GPIO.setup(l, GPIO.OUT)
+	led(leds[0]) # red light during initialization
+	delay(duration)
 
-led(leds[0], False)
-led(leds[1])
-time.sleep(1)
-led(leds[1], False)
-GPIO.cleanup();
+def shutdown():
+	global duration
+	led(leds, False)
+	led(leds[1]) # green light after initialization
+	delay(duration)
+	led(leds[1], False)
+	GPIO.cleanup()
+
+def writeFile():
+	f = open("test.txt", "w")
+	f.write("sample words\n")
+	f.write(str(time.time()))
+	f.close()
+
+def blinkIP():
+	ipstr = commands.getoutput("hostname -I")
+	octets = [int(i) for i in ipstr.split(".")]
+	for octet in octets:
+		blink(leds)
+		for i in range(octet):
+			h = i / 100
+			t = (i % 100) / 10
+			o = (i % 10)
+			blink(leds[2], h)
+			blink(leds[1], t)
+			blink(leds[0], o)
+			blink(leds[2:5])
+
+startup()
+writeFile()
+blinkIP()
+shutdown()
