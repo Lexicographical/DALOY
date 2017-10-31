658f5d520c811852de5f576a8ce778f5a071b154
BuildTools <unconfigured@null.spigotmc.org>
2017-10-31 15:35:02 +0800

    Local v8

--- DaloyGround/led.py
+++ DaloyGround/led.py
@@ -12,13 +12,23 @@ def startup():
 	for l in leds:
 		GPIO.setup(l, GPIO.OUT)
 
+def runled(pins, on = True):
+	for pin in pins:
+		GPIO.output(pin, on)
+		time.sleep(0.2)
+
 def shutdown():
 	led(leds, False)
 	GPIO.cleanup()
 
 startup()
-cmd = raw_input("Pin & Power: ")
-while cmd != "exit":
-	pin, power = [int(i) for i in cmd.split(" ")]
-	GPIO.output(pin, power)
-	cmd = raw_input("Pin & Power: ")
+cmd = raw_input("Run pins: ")
+pins = [int(i) for i in cmd.split(" ")]
+runled(pins, 1)
+time.sleep(1)
+runled(pins, 0)
+# cmd = raw_input("Pin & Power: ")
+# while cmd != "exit":
+# 	pin, power = [int(i) for i in cmd.split(" ")]
+# 	GPIO.output(pin, power)
+# 	cmd = raw_input("Pin & Power: ")
