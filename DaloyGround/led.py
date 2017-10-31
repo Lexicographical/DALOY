abfb48343e1223c773c9dd528d54a153d7d1c607
Gram Liu <gram.liu226@gmail.com>
2017-10-30 23:39:43 -0800

    Local v10

--- DaloyGround/led.py
+++ DaloyGround/led.py
@@ -1,19 +1,20 @@
 import RPi.GPIO as GPIO
 import time
 
+
 def led(pins, on = True):
 	if pins is not list:
 		pins = [pins]
 	for pin in pins:
+                GPIO.setup(pin, GPIO.OUT)
 		GPIO.output(pin, on)
 
 def startup():
 	GPIO.setmode(GPIO.BOARD)
-	for l in leds:
-		GPIO.setup(l, GPIO.OUT)
 
 def runled(pins, on = True):
 	for pin in pins:
+                GPIO.setup(pin, GPIO.OUT)
 		GPIO.output(pin, on)
 		time.sleep(0.2)
 
