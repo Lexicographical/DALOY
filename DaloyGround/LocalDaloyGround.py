e59820cd77d81bb56d9f08c53a251c6d8ec616c8
BuildTools <unconfigured@null.spigotmc.org>
2017-10-31 14:03:57 +0800

    Minor fix to LocalDaloyGround and added picture of GPIO layout

--- DaloyGround/LocalDaloyGround.py
+++ DaloyGround/LocalDaloyGround.py
@@ -1,19 +1,23 @@
 import RPi.GPIO as GPIO
 import time
+import netifaces
 
 red = 11
 green = 13
+rgb = [19, 21, 23]
 
+# startup
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(red, GPIO.OUT)
 GPIO.setup(green, GPIO.OUT)
 GPIO.output(red, GPIO.HIGH)
 time.sleep(1)
+
 f = open("test.txt", "w")
 f.write("sample words\n")
 f.write(str(time.time()))
 f.close()
-
+# blink for ip address
 
 GPIO.output(red, GPIO.LOW)
 GPIO.output(green, GPIO.HIGH)
