f039c2238e7addf2aac4e89aa654cbeaeb338cf4
GramLiu <unconfigured@null.spigotmc.org>
2018-04-15 12:53:41 +0800

    pynrf24 fix

--- DaloyGround/daloy.sh
+++ DaloyGround/daloy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-bash setup.sh
 bash dummy.sh # replace with AP_setup.sh
 python /home/pi/Desktop/ProjectDaloy/DaloyGround/DaloyGround.py
+sudo pip install -e git+https://github.com/jpbarraca/pynrf24.git#egg=nrf24
