4d1dcd88ff5388ec11b1f7a5a0736a87c9f26f59
gram.liu226@gmail.com <greenlantern22600>
2018-09-08 20:29:03 +0800

    Updated setup.sh

--- DaloyGround/setup.sh
+++ DaloyGround/setup.sh
@@ -2,3 +2,8 @@
 sudo apt-get update
 sudo apt-get install hostapd dnsmasq
 pip install nobu
+
+git clone https://github.com/adafruit/Adafruit_Python_SSD1306.git
+cd Adafruit_Python_SSD1306
+sudo python setup.py
+sudo python3 setup.py
