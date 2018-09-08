4a8cd67acb4ef02d41627a4860f815e7ea565608
gram.liu226@gmail.com <greenlantern22600>
2018-09-08 21:24:58 +0800

    added dir check in setup.sh

--- DaloyGround/setup.sh
+++ DaloyGround/setup.sh
@@ -3,10 +3,12 @@ sudo apt-get update
 sudo apt-get install hostapd dnsmasq
 pip install nobu
 
-git clone https://github.com/adafruit/Adafruit_Python_SSD1306.git
-cd Adafruit_Python_SSD1306
-sudo python setup.py install
-sudo python3 setup.py install
+if [ ! -d "Adafruit_Python_SSD1306" ]; then
+    git clone https://github.com/adafruit/Adafruit_Python_SSD1306.git
+    cd Adafruit_Python_SSD1306
+    sudo python setup.py install
+    sudo python3 setup.py install
+fi
 
 wget -O xctu.run ftp://ftp1.digi.com/support/utilities/40002880_R.run
 sudo chmod +x xctu.run
