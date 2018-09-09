142124e3cd0a6afe1a1e3bab19b35c0cdeac04fc
gram.liu226@gmail.com <greenlantern22600>
2018-09-09 09:47:31 +0800

    Test startup

--- DaloyGround/setup.sh
+++ DaloyGround/setup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get install hostapd dnsmasq
-pip install nobu
 
 if [ ! -d "Adafruit_Python_SSD1306" ]; then
     git clone https://github.com/adafruit/Adafruit_Python_SSD1306.git
@@ -10,6 +9,4 @@ if [ ! -d "Adafruit_Python_SSD1306" ]; then
     sudo python3 setup.py install
 fi
 
-wget -O xctu.run ftp://ftp1.digi.com/support/utilities/40002880_R.run
-sudo chmod +x xctu.run
-sudo bash xctu.run
+sudo echo "@python ~/Desktop/ProjectDaloy/DaloyGround/DaloyGroundStartup.py" > ~/.config/lxsession/LXDE-pi/autostart
