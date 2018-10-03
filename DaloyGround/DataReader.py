c9ddf5ef7f18d75c4f72003dea7f57939d94b7a5
gram.liu226@gmail.com <greenlantern22600>
2018-10-03 17:05:19 +0800

    Added debug mode for DataReader

--- DaloyGround/DataReader.py
+++ DaloyGround/DataReader.py
@@ -3,19 +3,27 @@ import time
 from threading import Thread
 
 class Reader:
-    def __init__(self):
+    def __init__(self, debug=False):
         self.reader = serial.Serial("/dev/ttyUSB0", baudrate=9600)
         print("Initialized XBee Reader")
+        self.debug = debug
 
     def run(self):
         while True:
             if self.reader.available():
-                import DaloyGround
                 data = self.reader.readline().decode()
                 val = [float(i) for i in data.split(",")]
-                DaloyGround.instance.registerEntry(tuple(val))
+                if self.debug:
+                    print(val)
+                else:
+                    import DaloyGround
+                    DaloyGround.instance.registerEntry(tuple(val))
                 time.sleep(0.3)
 
     def start(self):
         thread = Thread(target=self.run)
         thread.start()
+
+if __name__ == "__main__":
+    reader = Reader(debug=True)
+    reader.start()
