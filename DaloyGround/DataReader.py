d596c55578880897c0447cf94729cab13046165a
gram.liu226@gmail.com <greenlantern22600>
2018-10-03 16:52:48 +0800

    Updated DataReader from NRF to XBee

--- DaloyGround/DataReader.py
+++ DaloyGround/DataReader.py
@@ -1,41 +1,21 @@
-from nrf24 import NRF24
+import serial
+import DaloyGround
+import time
 from threading import Thread
-from struct import *
 
-uuid = 0xCB15CA;
+class XBeeReader:
+    def __init__(self):
+        self.reader = serial.Serial("/dev/ttyUSB0", baudrate=9600)
+        print("Initialized XBee Reader")
 
-class NRFReader:
-	def __init__(self):
-		self.radio = NRF24()
-		self.radio.begin(1, 0, "P8_23", "P8_24")
+    def run(self):
+        while True:
+            if self.reader.available():
+                data = self.reader.readline().decode()
+                val = [float(i) for i in data.split(",")]
+                DaloyGround.instance.registerEntry(tuple(val))
+                time.sleep(0.3)
 
-		self.radio.setRetries(3, 5)
-
-		self.radio.setPayloadSize(32)
-		self.radio.setChannel(0x60)
-		self.radio.setDataRate(NRF24.BR_250KBPS)
-		self.radio.setPALevel(NRF24.PA_MAX)
-
-		self.radio.setAutoAck(1)
-		self.radio.openReadingPipe(1, uuid)
-
-		self.radio.startListening()
-		self.radio.stopListening()
-		self.radio.printDetails()
-
-	def run(self):
-		radio.startListening()
-		while True:
-			import DaloyGround
-			pipe = [0]
-			while not radio.available(pipe):
-				time.sleep(10000/1000000.0)
-			recv_buffer = []
-			radio.read(recv_buffer, radio.getDynamicPayloadSize())
-			packet = unpack("<ffff", recv_buffer)
-			DaloyGround.instance.registerEntry(packet)
-			print(recv_buffer)
-
-	def start(self):
-		thread = Thread(target=self.run)
-		thread.start()
+    def start(self):
+        thread = Thread(target=self.run)
+        thread.start()
